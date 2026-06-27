3bb6cb68d363bd8f3e55f6593341998398213780
isamassarioli <93159761+isamassarioli@users.noreply.github.com>
2026-06-27 15:13:53 -0300

    Atualização4

--- loja-virtual-angular/src/app/pages/product-detail/product-detail.ts
+++ loja-virtual-angular/src/app/pages/product-detail/product-detail.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core'; // 1. Importar o ChangeDetectorRef
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { ApiService } from '../../services/api';
@@ -15,14 +15,18 @@ export class ProductDetailComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private apiService: ApiService
+    private apiService: ApiService,
+    private cdr: ChangeDetectorRef // 2. Injetar o ChangeDetectorRef no construtor
   ) {}
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.apiService.getProductById(id).subscribe({
-        next: (data) => this.product = data,
+        next: (data) => {
+          this.product = data;
+          this.cdr.detectChanges(); // 3. Avisar o Angular para renderizar a página imediatamente!
+        },
         error: (err) => console.error(err)
       });
     }
